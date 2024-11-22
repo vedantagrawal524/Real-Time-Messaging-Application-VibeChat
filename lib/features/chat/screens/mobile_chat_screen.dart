e01298483abdb5f8a733d55853e282e68af9a649
Vedant Agrawal <96226581+vedantagrawal524@users.noreply.github.com>
2024-11-23 02:09:39 +0530

    Messaging App Error Fix

--- lib/features/chat/screens/mobile_chat_screen.dart
+++ lib/features/chat/screens/mobile_chat_screen.dart
@@ -4,6 +4,7 @@ import 'package:whatsapp/common/utils/colors.dart';
 import 'package:whatsapp/common/widgets/loader.dart';
 import 'package:whatsapp/features/auth/controller/auth_controller.dart';
 import 'package:whatsapp/features/call/controller/call_controller.dart';
+import 'package:whatsapp/features/call/screens/call_pickup_screen.dart';
 import 'package:whatsapp/features/chat/widgets/bottom_chat_field.dart';
 import 'package:whatsapp/models/user_model.dart';
 import 'package:whatsapp/features/chat/widgets/chat_list.dart';
@@ -34,74 +35,76 @@ class MobileChatScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: appBarColor,
-        title: isGroupChat
-            ? Text(
-                name,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(fontSize: 18.5),
-              )
-            : StreamBuilder<UserModel>(
-                stream: ref.read(authControllerProvider).userDataById(uid),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Loader();
-                  }
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        name,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(fontSize: 18.5),
-                      ),
-                      Text(
-                        snapshot.data!.isOnline ? 'online' : 'offline',
-                        style: const TextStyle(
-                          fontSize: 13,
-                          fontWeight: FontWeight.normal,
+    return CallPickupScreen(
+      scaffold: Scaffold(
+        appBar: AppBar(
+          backgroundColor: appBarColor,
+          title: isGroupChat
+              ? Text(
+                  name,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(fontSize: 18.5),
+                )
+              : StreamBuilder<UserModel>(
+                  stream: ref.read(authControllerProvider).userDataById(uid),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Loader();
+                    }
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          name,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontSize: 18.5),
                         ),
-                      ),
-                    ],
-                  );
-                },
+                        Text(
+                          snapshot.data!.isOnline ? 'online' : 'offline',
+                          style: const TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+          centerTitle: false,
+          actions: [
+            IconButton(
+                onPressed: () => makeCall(context, ref),
+                icon: const Icon(
+                  Icons.video_call,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.call,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.more_vert,
+                )),
+          ],
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ChatList(
+                receiverUserId: uid,
+                isGroupChat: isGroupChat,
               ),
-        centerTitle: false,
-        actions: [
-          IconButton(
-              onPressed: () => makeCall(context, ref),
-              icon: const Icon(
-                Icons.video_call,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.call,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.more_vert,
-              )),
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ChatList(
+            ),
+            BottomChatField(
               receiverUserId: uid,
               isGroupChat: isGroupChat,
             ),
-          ),
-          BottomChatField(
-            receiverUserId: uid,
-            isGroupChat: isGroupChat,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
